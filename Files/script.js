8d9e9bce1d5a2fa4471dae6ce90ba30417c8af88
iMac <tkondrashin@icloud.com>
2020-12-17 22:35:36 +0300

    1

--- Files/script.js
+++ Files/script.js
@@ -12,13 +12,13 @@ $(window).scroll(function () {
         $('.logo img').css('filter', 'invert(0)');
         //$('.bg-video').css('display', 'block');
     }
-    else if (window.scrollY > (window.screen.height + (document.querySelector('.our-history').offsetHeight - 142))) {
+    if (window.scrollY > (window.screen.height + (document.querySelector('.our-history').offsetHeight - 142))) {
         $('.header').css('background-color', 'rgba(245,245,247,0.8)');
         $('div.submenu a').css('color', 'black');
         $('.logo img').css('filter', 'invert(1)');
         //$('.bg-video').css('display', 'none');
     }
-    else if (window.scrollY > (window.screen.height + 2558)) {
+    if (window.scrollY > (window.screen.height + 2558)) {
         $('.header').css('background-color', 'rgba(52,52,52,0.9)');
         $('div.submenu a').css('color', 'white');
         $('.logo img').css('filter', 'invert(0)');
